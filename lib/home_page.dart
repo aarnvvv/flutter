a94a9c742ddae931987e8d807223382a98aff4ef
Aarnav <aarnavbharadwaj05@gmail.com>
2023-12-05 00:16:45 +0530

    day 2 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  final int days = 30; //can only take integers
+  final String name = "xyz"; //can only take strings
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Catalog App",),
+          backgroundColor: Colors.blue,
+        ),
+        body: Center(
+          child: Container(
+          child: Text("Welcome to $days days of flutter by $name"),
+              ),
+        ),
+      drawer: Drawer(),
+      );
+  }
+}
