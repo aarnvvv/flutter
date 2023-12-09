c5adfe79d1be745028c02bcbef036c0078db1b0c
Aarnav <aarnavbharadwaj05@gmail.com>
2023-12-09 21:59:57 +0530

    day 3 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,7 +10,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: Text("Catalog App",),
-          backgroundColor: Colors.blue,
+  
         ),
         body: Center(
           child: Container(
